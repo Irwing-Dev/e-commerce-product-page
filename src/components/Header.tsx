8fb0b8ddf781efc4ff44b955615ffacff8db0f4f
Irwing <irwing.devbr@gmail.com>
2024-02-06 20:30:29 -0300

    feat: add mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface HeaderProps {
     logo: string;
@@ -6,51 +6,72 @@ interface HeaderProps {
     userAvatar: string;
 }
 
+interface INavProps {
+    text: string;
+}
+
+function NavBtns(props: INavProps) {
+    return (
+        <a href="#" className="text-gray-800 hover:text-gray-900">
+            {props.text}
+        </a>
+    );
+}
+
 const Header: React.FC<HeaderProps> = ({ logo, cart, userAvatar }) => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const handleMobileMenuToggle = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
     return (
         <div className="bg-white w-10/12 p-3 fixed shadow-md">
             <div className="max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex items-center justify-between h-16">
-                    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                        {/* Mobile menu button */}
+                    <div className="absolute inset-y-0 left-0 flex items-center">
+                        {/* Mobile menu toggle button */}
+                        <button
+                            className="text-gray-800 hover:text-gray-900 sm:hidden"
+                            onClick={handleMobileMenuToggle}
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                className="h-6 w-6"
+                            >
+                                {isMobileMenuOpen ? (
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth={2}
+                                        d="M6 18L18 6M6 6l12 12"
+                                    />
+                                ) : (
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth={2}
+                                        d="M4 6h16M4 12h16M4 18h16"
+                                    />
+                                )}
+                            </svg>
+                        </button>
                     </div>
                     <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start gap-3">
                         <div className="flex-shrink-0">
-                            <img src={logo} alt="" />
+                            <img src={logo} alt="Sneakers" />
                         </div>
                         <div className="hidden sm:block sm:ml-6">
-                            <div className="flex space-x-4">
-                                <a
-                                    href="#"
-                                    className="text-gray-800 hover:text-gray-900"
-                                >
-                                    Collections
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-800 hover:text-gray-900"
-                                >
-                                    Men
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-800 hover:text-gray-900"
-                                >
-                                    Womam
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-800 hover:text-gray-900"
-                                >
-                                    About
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-800 hover:text-gray-900"
-                                >
-                                    Contact
-                                </a>
-                            </div>
+                            <nav className="space-x-4 flex">
+                                <NavBtns text="Collections" />
+                                <NavBtns text="Men" />
+                                <NavBtns text="Womem" />
+                                <NavBtns text="About" />
+                                <NavBtns text="Contact" />
+                            </nav>
                         </div>
                     </div>
                     <div className="relative">
@@ -71,6 +92,20 @@ const Header: React.FC<HeaderProps> = ({ logo, cart, userAvatar }) => {
                         </div>
                     </div>
                 </div>
+            </div>
+            {/* Mobile menu component */}
+            <div
+                className={`${
+                    isMobileMenuOpen ? "block" : "hidden"
+                } sm:hidden absolute left-0 w-full h-auto bg-gray-200`}
+            >
+                <nav className="p-5">
+                    <NavBtns text="Collections" />
+                    <NavBtns text="Men" />
+                    <NavBtns text="Womem" />
+                    <NavBtns text="About" />
+                    <NavBtns text="Contact" />
+                </nav>
             </div>
         </div>
     );
