6ecd004c5e13ad0f5381c0beb7709ceae27626df
Irwing <irwing.devbr@gmail.com>
2024-02-10 20:22:18 -0300

    feat: add alias import
    I was wanting test more of vim terminal and improve my search for files
    
    I've added settings in vite and tsconfig.json for better research

--- src/components/ProductSection.tsx
+++ src/components/ProductSection.tsx
@@ -1,12 +1,12 @@
-import ProductImg1 from '../assets/images/image-product-1.jpg';
-import ProductImg2 from '../assets/images/image-product-2.jpg';
-import ProductImg3 from '../assets/images/image-product-3.jpg';
-import ProductImg4 from '../assets/images/image-product-4.jpg';
+import ProductImg1 from '@/assets/images/image-product-1.jpg';
+import ProductImg2 from '@/assets/images/image-product-2.jpg';
+import ProductImg3 from '@/assets/images/image-product-3.jpg';
+import ProductImg4 from "@/assets/images/image-product-4.jpg"
 
-import ProductImgThumb1 from '../assets/images/image-product-1-thumbnail.jpg';
-import ProductImgThumb2 from '../assets/images/image-product-2-thumbnail.jpg';
-import ProductImgThumb3 from '../assets/images/image-product-3-thumbnail.jpg';
-import ProductImgThumb4 from '../assets/images/image-product-4-thumbnail.jpg';
+import ProductImgThumb1 from '@/assets/images/image-product-1-thumbnail.jpg';
+import ProductImgThumb2 from '@/assets/images/image-product-2-thumbnail.jpg';
+import ProductImgThumb3 from '@/assets/images/image-product-3-thumbnail.jpg';
+import ProductImgThumb4 from '@/assets/images/image-product-4-thumbnail.jpg';
 
 import { ImageSlider } from './ImageSlider';
 
