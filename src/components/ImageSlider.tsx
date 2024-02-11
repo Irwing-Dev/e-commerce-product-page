afea1705e971407fd6dda040baa8412208a4b66a
Irwing <irwing.devbr@gmail.com>
2024-02-11 16:27:23 -0300

    feat: add carousel with shadcn-ui
    I added a carousel because in the design is required, i opted for
    shadcn-ui just because for me was the better optionI added a carousel
    because in the design it's necessary, I opted for
    shadcn-UI just because for me it was the best option, taking into
    account that I wanted to finish it as soon as possible
    
    This feature add a carousel with a dialog

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,54 +1,138 @@
 import { useState } from "react";
 
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import {
+    Carousel,
+    CarouselItem,
+    CarouselContent,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel";
+
+import ProductImg1 from "@/assets/images/image-product-1.jpg";
+import ProductImg2 from "@/assets/images/image-product-2.jpg";
+import ProductImg3 from "@/assets/images/image-product-3.jpg";
+import ProductImg4 from "@/assets/images/image-product-4.jpg";
+
 interface ImageSliderProps {
-  images: string[];
-  thumbnails: string[];
+    images: string[];
+    thumbnails: string[];
 }
 
 export const ImageSlider: React.FC<ImageSliderProps> = ({
-  images,
-  thumbnails,
+    images,
+    thumbnails,
 }) => {
-  const [selectedIndex, setSelectedIndex] = useState(0);
+    const [selectedIndex, setSelectedIndex] = useState(0);
 
-  const handleImageClick = (index: number) => {
-    setSelectedIndex(index);
-  };
+    const handleImageClick = (index: number) => {
+        setSelectedIndex(index);
+    };
 
-  return (
-    <div className="flex flex-col gap-3">
-      <div className="flex">
-        {images.map((image, index) => (
-          <img
-            key={image}
-            src={image}
-            alt="ProductImg"
-            height={320}
-            width={320}
-            className={`rounded-md ${selectedIndex === index ? "" : "hidden"}`}
-            data-testid={`product-img-${index + 1}`}
-          />
-        ))}
-      </div>
-      <div className="flex gap-3">
-        {thumbnails.map((thumbnail, index) => (
-          <div key={thumbnail} className="relative">
-            <img
-              src={thumbnail}
-              alt=""
-              height={70}
-              width={70}
-              className="rounded-md cursor-pointer"
-              onClick={() => handleImageClick(index)}
-              data-testid={`thumb-${index + 1}`}
-            />
-            <span
-              className={`absolute bg-[#dddddd99] w-full h-full top-0 border-solid rounded-md cursor-pointer border-2 border-[#ff7d1a] ${selectedIndex === index ? "flex" : "hidden"}`}
-              data-testid={`thumb-lightbox-${index + 1}`}
-            ></span>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex flex-col gap-2 justify-center items-center">
+            <div className="flex justify-center items-center">
+                <Dialog>
+                    <DialogTrigger>
+                        {images.map((image, index) => (
+                            <Button className="bg-transparent w-aut h-auto hover:bg-transparent px-0">
+                                <img
+                                    key={image}
+                                    src={image}
+                                    alt="ProductImg"
+                                    height={340}
+                                    className={`rounded-md ${
+                                        selectedIndex === index ? "" : "hidden"
+                                    } sm:w-[400px]`}
+                                    data-testid={`product-img-${index + 1}`}
+                                />
+                            </Button>
+                        ))}
+                    </DialogTrigger>
+                    <DialogContent className="bg-transparent border-none p-12">
+                        <Carousel opts={{ loop: true }}>
+                            <CarouselContent>
+                                <CarouselItem className="flex items-center justify-center">
+                                    <Card className="bg-transparent p-0 border-none">
+                                        <CardContent>
+                                            <img
+                                                src={ProductImg1}
+                                                alt="ProductImage"
+                                                className="rounded-md"
+                                                height={320}
+                                                width={320}
+                                            />
+                                        </CardContent>
+                                    </Card>
+                                </CarouselItem>
+                                <CarouselItem className="flex items-center justify-center">
+                                    <Card className="bg-transparent p-0 border-none">
+                                        <CardContent>
+                                            <img
+                                                src={ProductImg2}
+                                                alt="ProductImage"
+                                                className="rounded-md"
+                                                height={320}
+                                                width={320}
+                                            />
+                                        </CardContent>
+                                    </Card>
+                                </CarouselItem>
+                                <CarouselItem className="flex items-center justify-center">
+                                    <Card className="bg-transparent p-0 border-none">
+                                        <CardContent>
+                                            <img
+                                                src={ProductImg3}
+                                                alt="ProductImage"
+                                                className="rounded-md"
+                                                height={320}
+                                                width={320}
+                                            />
+                                        </CardContent>
+                                    </Card>
+                                </CarouselItem>
+                                <CarouselItem className="flex items-center justify-center">
+                                    <Card className="bg-transparent p-0 border-none">
+                                        <CardContent>
+                                            <img
+                                                src={ProductImg4}
+                                                alt="ProductImage"
+                                                className="rounded-md"
+                                                height={320}
+                                                width={320}
+                                            />
+                                        </CardContent>
+                                    </Card>
+                                </CarouselItem>
+                            </CarouselContent>
+                            <CarouselPrevious />
+                            <CarouselNext />
+                        </Carousel>
+                    </DialogContent>
+                </Dialog>
+            </div>
+            <div className="flex gap-8">
+                {thumbnails.map((thumbnail, index) => (
+                    <div key={thumbnail} className="relative">
+                        <img
+                            src={thumbnail}
+                            alt=""
+                            height={90}
+                            className="rounded-md cursor-pointer sm:w-20"
+                            onClick={() => handleImageClick(index)}
+                            data-testid={`thumb-${index + 1}`}
+                        />
+                        <span
+                            className={`absolute bg-[#e6e2e2d8] w-full h-full top-0 border-solid rounded-md cursor-pointer border-2 border-[#ff7d1a] ${
+                                selectedIndex === index ? "flex" : "hidden"
+                            }`}
+                            data-testid={`thumb-lightbox-${index + 1}`}
+                        ></span>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
--- src/components/ProductSection.tsx
+++ src/components/ProductSection.tsx
@@ -1,29 +1,34 @@
-import ProductImg1 from '@/assets/images/image-product-1.jpg';
-import ProductImg2 from '@/assets/images/image-product-2.jpg';
-import ProductImg3 from '@/assets/images/image-product-3.jpg';
-import ProductImg4 from "@/assets/images/image-product-4.jpg"
+import ProductImg1 from "@/assets/images/image-product-1.jpg";
+import ProductImg2 from "@/assets/images/image-product-2.jpg";
+import ProductImg3 from "@/assets/images/image-product-3.jpg";
+import ProductImg4 from "@/assets/images/image-product-4.jpg";
 
-import ProductImgThumb1 from '@/assets/images/image-product-1-thumbnail.jpg';
-import ProductImgThumb2 from '@/assets/images/image-product-2-thumbnail.jpg';
-import ProductImgThumb3 from '@/assets/images/image-product-3-thumbnail.jpg';
-import ProductImgThumb4 from '@/assets/images/image-product-4-thumbnail.jpg';
+import ProductImgThumb1 from "@/assets/images/image-product-1-thumbnail.jpg";
+import ProductImgThumb2 from "@/assets/images/image-product-2-thumbnail.jpg";
+import ProductImgThumb3 from "@/assets/images/image-product-3-thumbnail.jpg";
+import ProductImgThumb4 from "@/assets/images/image-product-4-thumbnail.jpg";
 
-import { ImageSlider } from './ImageSlider';
+import { ImageSlider } from "./ImageSlider";
 
 export default function TestSection() {
-  return (
-    <div className="bg-gray-100 min-h-screen w-5/6 flex items-center justify-around ">
-      <div className="grid gap-3">
-        <ImageSlider
-          images={[ProductImg1, ProductImg2, ProductImg3, ProductImg4]}
-          thumbnails={[
-            ProductImgThumb1,
-            ProductImgThumb2,
-            ProductImgThumb3,
-            ProductImgThumb4,
-          ]}
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div className="min-h-screen w-5/6 grid lg:flex items-center justify-evenly mt-24 lg:mt-4">
+            <div className="grid mb-12">
+                <ImageSlider
+                    images={[
+                        ProductImg1,
+                        ProductImg2,
+                        ProductImg3,
+                        ProductImg4,
+                    ]}
+                    thumbnails={[
+                        ProductImgThumb1,
+                        ProductImgThumb2,
+                        ProductImgThumb3,
+                        ProductImgThumb4,
+                    ]}
+                />
+            </div>
+        </div>
+    );
 }
