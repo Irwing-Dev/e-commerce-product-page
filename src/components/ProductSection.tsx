669a79e8a992d576292cf2c60865c55048f9953d
Irwing <irwing.devbr@gmail.com>
2024-02-11 16:32:44 -0300

    feat: add the product description
    I added the product description as it is requested in the design
    
    This feature add the product description

--- src/components/ProductSection.tsx
+++ src/components/ProductSection.tsx
@@ -8,6 +8,10 @@ import ProductImgThumb2 from "@/assets/images/image-product-2-thumbnail.jpg";
 import ProductImgThumb3 from "@/assets/images/image-product-3-thumbnail.jpg";
 import ProductImgThumb4 from "@/assets/images/image-product-4-thumbnail.jpg";
 
+import CartIcon from "@/assets/images/icon-cart.svg";
+
+import { Button } from "@/components/ui/button";
+
 import { ImageSlider } from "./ImageSlider";
 
 export default function TestSection() {
@@ -28,6 +32,52 @@ export default function TestSection() {
                         ProductImgThumb4,
                     ]}
                 />
+            </div>
+            <div className="">
+                <h3 className="text-md uppercase tracking-widest text-[#ff7d1a] font-bold mb-3">
+                    Sneaker Company
+                </h3>
+                <h1 className="text-5xl max-w-96 font-bold mb-8">
+                    Fall Limited Edition Sneakers
+                </h1>
+                <p className="max-w-[410px] text-[#68707d] mb-6">
+                    These low-profile sneakers are your perfect casual wear
+                    companion. Featuring a durable rubber outer stole,
+                    they&rsquo;ll withstand everything the weather can offer.
+                </p>
+                <div className="flex justify-between lg:grid">
+                    <div className="flex gap-3 mb-2">
+                        <h2 className="text-3xl font-bold">$125.00</h2>
+                        <span className="text-[#ff7d1a] bg-[#ffede0] font-bold px-2 text-center h-7 rounded-lg">
+                            50&#x00025;
+                        </span>
+                    </div>
+                    <s className="text-[#b6bcc8] font-bold">$250.00</s>
+                </div>
+                <div>
+                    <div className="flex items-center mt-2 w-full p-2">
+                        <Button className="bg-[#f7f8fd] hover:bg-#f7f8fd text-[#ff7d1a] text-lg font-bold px-5 w-full lg:w-auto rounded-tr-none rounded-br-none">
+                            &minus;
+                        </Button>
+                        <p className="font-bold py-2 px-5 w-full text-center lg:w-auto bg-[#f7f8fd]">
+                            0
+                        </p>
+                        <Button className="bg-[#f7f8fd] hover:bg-#f7f8fd text-[#ff7d1a] text-lg font-bold px-5 w-full lg:w-auto rounded-tl-none rounded-bl-none">
+                            &#x0002B;
+                        </Button>
+                    </div>
+                    {/* <!-- Add to cart button --> */}
+                    <Button className="w-full lg:w-52 h-12 bg-[#ff7d1a] hover:bg-[#ff7d1a98] flex justify-evenly" >
+                        <img
+                            src={CartIcon}
+                            alt="CartIcon"
+                            className="brightness-200"
+                            height={17}
+                            width={17}
+                        />
+                        <span>Add to cart</span>
+                    </Button>
+                </div>
             </div>
         </div>
     );
